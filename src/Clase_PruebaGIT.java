621b2b7335dc306c9b37e65ff22d19af7b04022f
Siwrad <darwisnarvaezp@gmail.com>
2021-02-07 20:00:12 -0300

    Agregué te pusiste muñeca

--- src/Clase_PruebaGIT.java
+++ src/Clase_PruebaGIT.java
@@ -5,7 +5,8 @@ public class Clase_PruebaGIT {
 	public static void main(String[] args) {
 
 		JOptionPane.showMessageDialog(null, "Diablo diablooona");
-		
+		JOptionPane.showMessageDialog(null, "Te pusiste muñeca");
+
 	}
 
 }
